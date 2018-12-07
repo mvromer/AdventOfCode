f181aa3f8d058d274312980e6157946c7062f2a8
Michael Romer <mvromer@gmail.com>
2018-12-06 21:19:36 -0600

    Added shared code to separate library crate

---
+++ 2018/aoc_shared/src/lib.rs
@@ -0,0 +1,20 @@
+extern crate clap;
+use clap::{Arg, App};
+
+pub struct CommandLine {
+    pub input_file_name: String
+}
+
+pub fn parse_command_line( title: &str ) -> CommandLine {
+    let matches = App::new( title )
+        .arg( Arg::with_name( "input" )
+            .help( "Input file to use" )
+            .required( true )
+            .index( 1 )
+        )
+        .get_matches();
+
+    CommandLine {
+        input_file_name: matches.value_of( "input" ).unwrap().to_string()
+    }
+}
