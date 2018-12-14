3e459ea15bb5c0b18f035bf8aa5a9ffd23ee5466
Michael Romer <mvromer@gmail.com>
2018-12-13 22:03:00 -0600

    Refactored for Day 4b

--- 2018/Day04/day4a/src/main.rs
+++ 2018/Day04/day4a/src/main.rs
@@ -1,7 +1,6 @@
-use aoc_shared::day4::{self, GuardEvent, GuardEvent::*};
-use chrono::naive::{NaiveDateTime, NaiveDate};
-use chrono::Timelike;
-use std::collections::{BTreeMap, HashMap};
+use aoc_shared::day4::{self, GuardEvent, SleepCounts};
+use chrono::naive::NaiveDateTime;
+use std::collections::BTreeMap;
 
 fn main() {
     let cli = aoc_shared::parse_command_line( "Advent of Code Day 4a" );
@@ -12,39 +11,10 @@ fn main() {
 }
 
 fn find_sleepiest_guard_checksum( guard_events: &BTreeMap<NaiveDateTime, GuardEvent> ) -> i32 {
-    let mut current_guard = 0;
-    let mut total_minutes_asleep = HashMap::new();
-    let mut per_minute_sleep_counts = HashMap::new();
-    let mut nap_start = NaiveDate::from_ymd( 1518, 1, 1 ).and_hms( 0, 0, 0 );
-
-    // Count up how many minutes each guard slept.
-    for (current_timestamp, guard_event) in guard_events.iter() {
-        match guard_event {
-            BeginShift( guard_id ) => current_guard = *guard_id,
-            FallsAsleep => nap_start = *current_timestamp,
-            WakesUp => {
-                // Add the number of minutes for the most recent nap.
-                let number_current_nap_minutes = (*current_timestamp - nap_start).num_minutes();
-                total_minutes_asleep.entry( current_guard )
-                    .and_modify( |current_minutes_asleep| {
-                        *current_minutes_asleep += number_current_nap_minutes;
-                    } )
-                    .or_insert( number_current_nap_minutes );
-
-                // We're told in the puzzle that all naps happen between 00:00 and 00:59, so for
-                // the current guard increment a per-minute counter so we track for which minute the
-                // guard was asleep the longest.
-                let start_minute = nap_start.minute() as usize;
-                let stop_minute = current_timestamp.minute() as usize;
-                let current_guard_per_minute_sleep_counts = per_minute_sleep_counts.entry( current_guard )
-                    .or_insert_with( || [0; 60] );
-
-                for minute in start_minute..stop_minute {
-                    (*current_guard_per_minute_sleep_counts)[minute] += 1;
-                }
-            }
-        }
-    }
+    let SleepCounts {
+        total_minutes_asleep,
+        per_minute_sleep_counts
+    } = day4::get_sleep_counts( guard_events );
 
     // Find the sleepiest guard, i.e., the one who slept the most number of minutes.
     let sleepiest_guard = total_minutes_asleep.iter()
--- 2018/aoc_shared/src/day4.rs
+++ 2018/aoc_shared/src/day4.rs
@@ -1,8 +1,9 @@
+use chrono::Timelike;
 use chrono::naive::{NaiveDateTime, NaiveDate};
 use crate::CommandLine;
 use crate::error::{MatchError, ProgramError};
 use regex::Regex;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 
@@ -11,6 +12,13 @@ pub enum GuardEvent {
     BeginShift( i32 ),
     FallsAsleep,
     WakesUp
+}
+
+use self::GuardEvent::*;
+
+pub struct SleepCounts {
+    pub total_minutes_asleep: HashMap<i32, i64>,
+    pub per_minute_sleep_counts: HashMap<i32, [i32; 60]>
 }
 
 pub fn read_guard_events( cli: &CommandLine ) -> Result<BTreeMap<NaiveDateTime, GuardEvent>, ProgramError> {
@@ -36,13 +44,54 @@ pub fn read_guard_events( cli: &CommandLine ) -> Result<BTreeMap<NaiveDateTime, 
 
         if let Some( begins_shift_captures ) = begins_shift_regex.captures( &line ) {
             let guard_id = crate::regex::convert_capture::<i32>( &begins_shift_captures, "guard_id" )?;
-            guard_events.insert( event_timestamp, GuardEvent::BeginShift( guard_id ) );
+            guard_events.insert( event_timestamp, BeginShift( guard_id ) );
         } else if line.contains( "falls asleep" ) {
-            guard_events.insert( event_timestamp, GuardEvent::FallsAsleep );
+            guard_events.insert( event_timestamp, FallsAsleep );
         } else if line.contains( "wakes up" ) {
-            guard_events.insert( event_timestamp, GuardEvent::WakesUp );
+            guard_events.insert( event_timestamp, WakesUp );
         }
     }
 
     Ok( guard_events )
+}
+
+pub fn get_sleep_counts( guard_events: &BTreeMap<NaiveDateTime, GuardEvent> ) -> SleepCounts {
+    let mut current_guard = 0;
+    let mut total_minutes_asleep = HashMap::new();
+    let mut per_minute_sleep_counts = HashMap::new();
+    let mut nap_start = NaiveDate::from_ymd( 1518, 1, 1 ).and_hms( 0, 0, 0 );
+
+    // Count up how many minutes each guard slept.
+    for (current_timestamp, guard_event) in guard_events.iter() {
+        match guard_event {
+            BeginShift( guard_id ) => current_guard = *guard_id,
+            FallsAsleep => nap_start = *current_timestamp,
+            WakesUp => {
+                // Add the number of minutes for the most recent nap.
+                let number_current_nap_minutes = (*current_timestamp - nap_start).num_minutes();
+                total_minutes_asleep.entry( current_guard )
+                    .and_modify( |current_minutes_asleep| {
+                        *current_minutes_asleep += number_current_nap_minutes;
+                    } )
+                    .or_insert( number_current_nap_minutes );
+
+                // We're told in the puzzle that all naps happen between 00:00 and 00:59, so for
+                // the current guard increment a per-minute counter so we track for which minute the
+                // guard was asleep the longest.
+                let start_minute = nap_start.minute() as usize;
+                let stop_minute = current_timestamp.minute() as usize;
+                let current_guard_per_minute_sleep_counts = per_minute_sleep_counts.entry( current_guard )
+                    .or_insert_with( || [0; 60] );
+
+                for minute in start_minute..stop_minute {
+                    (*current_guard_per_minute_sleep_counts)[minute] += 1;
+                }
+            }
+        }
+    }
+
+    SleepCounts {
+        total_minutes_asleep,
+        per_minute_sleep_counts
+    }
 }
