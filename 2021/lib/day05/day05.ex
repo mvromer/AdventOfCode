fea7b73ae88ba96acb9795a81e129461917f8dbf
Michael Romer <mvromer@gmail.com>
2021-12-06 21:57:25 -0600

    Finish day 5b

--- 2021/lib/day05/day05.ex
+++ 2021/lib/day05/day05.ex
@@ -21,12 +21,13 @@ defmodule Aoc2021.Day05 do
   end
 
   def solve_b do
-    @example_input
+    @puzzle_input
     |> File.stream!()
     |> Stream.map(&parse_line/1)
     |> Stream.flat_map(&get_covered_locations/1)
     |> Enum.frequencies()
-    |> Enum.sort(fn {la, _}, {lb, _} -> la <= lb end)
+    |> Enum.filter(fn {_, count} -> count >= 2 end)
+    |> Enum.count()
   end
 
   defp parse_line(line) do
@@ -44,24 +45,22 @@ defmodule Aoc2021.Day05 do
     end
   end
 
-  defp get_covered_locations(endpoints) do
+  defp get_covered_locations(%{x1: x1, y1: y1, x2: x2, y2: y2}) do
     cond do
-      endpoints.x1 == endpoints.x2 ->
+      x1 == x2 ->
         # Special case: vertical line.
-        minY = min(endpoints.y1, endpoints.y2)
-        maxY = max(endpoints.y1, endpoints.y2)
-
-        minY..maxY |> Stream.map(fn y -> {endpoints.x1, y} end)
+        ys = if y1 <= y2, do: y1..y2, else: y2..y1
+        ys |> Stream.map(fn y -> {x1, y} end)
 
       true ->
-        slope = (endpoints.y2 - endpoints.y1) / (endpoints.x2 - endpoints.x1)
-        minX = min(endpoints.x1, endpoints.x2)
-        maxX = max(endpoints.x1, endpoints.x2)
-        minY = if endpoints.x1 <= endpoints.x2, do: endpoints.y1, else: endpoints.y2
+        slope = (y2 - y1) / (x2 - x1)
+        xs = if x1 <= x2, do: x1..x2, else: x2..x1
 
         # In the case of horizontal/vertical lines and those whose slope is 1 (which the puzzle
-        # guarantees), we can safely truncate the calcualted y-coordinate.
-        minX..maxX |> Stream.map(fn x -> {x, trunc(minY + slope * x)} end)
+        # guarantees), we can safely truncate the calcualted y-coordinate. This ensures all our
+        # tuple elements are of the same data type (integers), which allows us to directly use
+        # Enum.frequencies for grouping.
+        xs |> Stream.map(fn x -> {x, trunc(y1 + slope * (x - x1))} end)
     end
   end
 end
