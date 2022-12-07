5f7d0eef0cc8008c795032e5c42ea181effa8253
Michael Romer <mvromer@gmail.com>
2022-12-07 00:15:17 -0600

    2022 - 2b

--- 2022/src/Day02.hs
+++ 2022/src/Day02.hs
@@ -10,62 +10,97 @@ import qualified Data.Text.Lazy.IO as TLIO
 data Move = Rock | Paper | Scissors
     deriving Show
 
+data Outcome = Win | Lose | Draw
+    deriving Show
+
 p2a :: PuzzleInput -> IO Int
-p2a inputType = sum . scoreRounds . readRounds . TL.lines <$> TLIO.readFile (puzzleInput inputType)
+p2a inputType = sum
+    . scoreRounds
+    . readRounds
+    . TL.lines
+    <$> TLIO.readFile (puzzleInput inputType)
 
 p2b :: PuzzleInput -> IO Int
-p2b inputType = return 0
+p2b inputType = sum
+    . scoreRounds
+    . convertToRounds
+    . readStrategy
+    . TL.lines
+    <$> TLIO.readFile (puzzleInput inputType)
 
 readRounds :: [TL.Text] -> [(Move, Move)]
 readRounds = map parseRound
     where
         parseRound :: TL.Text -> (Move, Move)
-        parseRound roundEntry = toRound . (map toMove) . TL.words $ roundEntry
+        parseRound entry = toRound . TL.words $ entry
 
-        toMove :: TL.Text -> Move
-        toMove move
-            | move == "A" || move == "X" = Rock
-            | move == "B" || move == "Y" = Paper
-            | move == "C" || move == "Z" = Scissors
-            | otherwise = error ("Invalid move " ++ (TL.unpack move))
+        toRound :: [TL.Text] -> (Move, Move)
+        toRound (x:y:[]) = (toMove x, toMove y)
+        toRound x = error ("Unexpected number of moves in " ++ (TL.unpack . TL.unwords $ x))
 
-        toRound :: [Move] -> (Move, Move)
-        toRound (x:y:[]) = (x,y)
-        toRound _ = error "Unexpected number of moves"
+        toMove x
+            | x == "A" || x == "X" = Rock
+            | x == "B" || x == "Y" = Paper
+            | x == "C" || x == "Z" = Scissors
+            | otherwise = error ("Invalid move " ++ (TL.unpack x))
+
+readStrategy :: [TL.Text] -> [(Move, Outcome)]
+readStrategy = map parseStrategy
+    where
+        parseStrategy :: TL.Text -> (Move, Outcome)
+        parseStrategy entry = toStrategy . TL.words $ entry
+
+        toStrategy :: [TL.Text] -> (Move, Outcome)
+        toStrategy (x:y:[]) = (toMove x, toOutcome y)
+        toStrategy x = error ("Unexpected number of entries in " ++ (TL.unpack . TL.unwords $ x))
+
+        toMove x
+            | x == "A" = Rock
+            | x == "B" = Paper
+            | x == "C" = Scissors
+            | otherwise = error ("Unexpected move " ++ (TL.unpack x))
+
+        toOutcome x
+            | x == "X" = Lose
+            | x == "Y" = Draw
+            | x == "Z" = Win
+            | otherwise = error ("Unexpected outcome " ++ (TL.unpack x))
+
+convertToRounds :: [(Move, Outcome)] -> [(Move, Move)]
+convertToRounds = map convertToRound
+    where
+        convertToRound (move, Draw) = (move, move)
+
+        convertToRound (Rock, Win) = (Rock, Paper)
+        convertToRound (Rock, Lose) = (Rock, Scissors)
+
+        convertToRound (Paper, Win) = (Paper, Scissors)
+        convertToRound (Paper, Lose) = (Paper, Rock)
+
+        convertToRound (Scissors, Win) = (Scissors, Rock)
+        convertToRound (Scissors, Lose) = (Scissors, Paper)
 
 scoreRounds :: [(Move, Move)] -> [Int]
 scoreRounds = map scoreRound
+    where
+        scoreRound (Rock, Rock) = rockPoints + tiePoints
+        scoreRound (Paper, Rock) = rockPoints + losePoints
+        scoreRound (Scissors, Rock) = rockPoints + winPoints
 
-scoreRound :: (Move, Move) -> Int
-scoreRound (Rock, Rock) = rockPoints + tiePoints
-scoreRound (Paper, Rock) = rockPoints + losePoints
-scoreRound (Scissors, Rock) = rockPoints + winPoints
+        scoreRound (Rock, Paper) = paperPoints + winPoints
+        scoreRound (Paper, Paper) = paperPoints + tiePoints
+        scoreRound (Scissors, Paper) = paperPoints + losePoints
 
-scoreRound (Rock, Paper) = paperPoints + winPoints
-scoreRound (Paper, Paper) = paperPoints + tiePoints
-scoreRound (Scissors, Paper) = paperPoints + losePoints
+        scoreRound (Rock, Scissors) = scissorsPoints + losePoints
+        scoreRound (Paper, Scissors) = scissorsPoints + winPoints
+        scoreRound (Scissors, Scissors) = scissorsPoints + tiePoints
 
-scoreRound (Rock, Scissors) = scissorsPoints + losePoints
-scoreRound (Paper, Scissors) = scissorsPoints + winPoints
-scoreRound (Scissors, Scissors) = scissorsPoints + tiePoints
-
-rockPoints :: Int
-rockPoints = 1
-
-paperPoints :: Int
-paperPoints = 2
-
-scissorsPoints :: Int
-scissorsPoints = 3
-
-losePoints :: Int
-losePoints = 0
-
-tiePoints :: Int
-tiePoints = 3
-
-winPoints :: Int
-winPoints = 6
+        rockPoints = 1
+        paperPoints = 2
+        scissorsPoints = 3
+        losePoints = 0
+        tiePoints = 3
+        winPoints = 6
 
 puzzleInput :: PuzzleInput -> String
 puzzleInput Example = "./data/day02/example.txt"
