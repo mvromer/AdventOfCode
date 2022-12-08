f1051914fe5caef90757f22a8ce3ba7323259de5
Michael Romer <mvromer@gmail.com>
2022-12-08 00:20:37 -0600

    Formatting

--- 2022/src/Day01.hs
+++ 2022/src/Day01.hs
@@ -4,13 +4,19 @@ module Day01 (p1a, p1b) where
 
 import Data.List
 import PuzzleInput
+
 import qualified Data.ByteString.Lazy.Char8 as BS
 
 p1a :: PuzzleInput -> IO Int
-p1a inputType = maximum . readCalories . BS.lines <$> BS.readFile (puzzleInput inputType)
+p1a inputType =
+  maximum
+    . readCalories
+    . BS.lines
+    <$> BS.readFile (puzzleInput inputType)
 
 p1b :: PuzzleInput -> IO Int
-p1b inputType = sum
+p1b inputType =
+  sum
     . take 3
     . reverse
     . sort
@@ -20,17 +26,17 @@ p1b inputType = sum
 
 readCalories :: [BS.ByteString] -> [Int]
 readCalories puzzle = lastElfCalories:caloriesPerElf
-    where
-        (caloriesPerElf, lastElfCalories) = foldl (updateCalories) ([], 0) puzzle
+  where
+    (caloriesPerElf, lastElfCalories) = foldl (updateCalories) ([], 0) puzzle
 
-        updateCalories :: ([Int], Int) -> BS.ByteString -> ([Int], Int)
-        updateCalories (currentCaloriesPerElf, currentElfCalories) EmptyByteString =
-            (currentElfCalories:currentCaloriesPerElf, 0)
+    updateCalories :: ([Int], Int) -> BS.ByteString -> ([Int], Int)
+    updateCalories (currentCaloriesPerElf, currentElfCalories) EmptyByteString =
+      (currentElfCalories:currentCaloriesPerElf, 0)
 
-        updateCalories (currentCaloriesPerElf, currentElfCalories) snackCalories =
-            case BS.readInt snackCalories of
-                Just (calories, _) -> (currentCaloriesPerElf, calories + currentElfCalories)
-                _ -> error ("Unexpected calorie" ++ (BS.unpack snackCalories))
+    updateCalories (currentCaloriesPerElf, currentElfCalories) snackCalories =
+      case BS.readInt snackCalories of
+        Just (calories, _) -> (currentCaloriesPerElf, calories + currentElfCalories)
+        _ -> error ("Unexpected calorie" ++ (BS.unpack snackCalories))
 
 pattern EmptyByteString :: BS.ByteString
 pattern EmptyByteString <- (BS.uncons -> Nothing)
--- 2022/src/Day02.hs
+++ 2022/src/Day02.hs
@@ -8,20 +8,22 @@ import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TLIO
 
 data Move = Rock | Paper | Scissors
-    deriving Show
+  deriving Show
 
 data Outcome = Win | Lose | Draw
-    deriving Show
+  deriving Show
 
 p2a :: PuzzleInput -> IO Int
-p2a inputType = sum
+p2a inputType =
+  sum
     . scoreRounds
     . readRounds
     . TL.lines
     <$> TLIO.readFile (puzzleInput inputType)
 
 p2b :: PuzzleInput -> IO Int
-p2b inputType = sum
+p2b inputType =
+  sum
     . scoreRounds
     . convertToRounds
     . readStrategy
@@ -30,77 +32,77 @@ p2b inputType = sum
 
 readRounds :: [TL.Text] -> [(Move, Move)]
 readRounds = map parseRound
-    where
-        parseRound :: TL.Text -> (Move, Move)
-        parseRound entry = toRound . TL.words $ entry
+  where
+    parseRound :: TL.Text -> (Move, Move)
+    parseRound entry = toRound . TL.words $ entry
 
-        toRound :: [TL.Text] -> (Move, Move)
-        toRound (x:y:[]) = (toMove x, toMove y)
-        toRound x = error ("Unexpected number of moves in " ++ (TL.unpack . TL.unwords $ x))
+    toRound :: [TL.Text] -> (Move, Move)
+    toRound (x:y:[]) = (toMove x, toMove y)
+    toRound x = error ("Unexpected number of moves in " ++ (TL.unpack . TL.unwords $ x))
 
-        toMove x
-            | x == "A" || x == "X" = Rock
-            | x == "B" || x == "Y" = Paper
-            | x == "C" || x == "Z" = Scissors
-            | otherwise = error ("Invalid move " ++ (TL.unpack x))
+    toMove x
+      | x == "A" || x == "X" = Rock
+      | x == "B" || x == "Y" = Paper
+      | x == "C" || x == "Z" = Scissors
+      | otherwise = error ("Invalid move " ++ (TL.unpack x))
 
 readStrategy :: [TL.Text] -> [(Move, Outcome)]
 readStrategy = map parseStrategy
-    where
-        parseStrategy :: TL.Text -> (Move, Outcome)
-        parseStrategy entry = toStrategy . TL.words $ entry
+  where
+    parseStrategy :: TL.Text -> (Move, Outcome)
+    parseStrategy entry = toStrategy . TL.words $ entry
 
-        toStrategy :: [TL.Text] -> (Move, Outcome)
-        toStrategy (x:y:[]) = (toMove x, toOutcome y)
-        toStrategy x = error ("Unexpected number of entries in " ++ (TL.unpack . TL.unwords $ x))
+    toStrategy :: [TL.Text] -> (Move, Outcome)
+    toStrategy (x:y:[]) = (toMove x, toOutcome y)
+    toStrategy x = error ("Unexpected number of entries in " ++ (TL.unpack . TL.unwords $ x))
 
-        toMove x
-            | x == "A" = Rock
-            | x == "B" = Paper
-            | x == "C" = Scissors
-            | otherwise = error ("Unexpected move " ++ (TL.unpack x))
+    toMove x
+      | x == "A" = Rock
+      | x == "B" = Paper
+      | x == "C" = Scissors
+      | otherwise = error ("Unexpected move " ++ (TL.unpack x))
 
-        toOutcome x
-            | x == "X" = Lose
-            | x == "Y" = Draw
-            | x == "Z" = Win
-            | otherwise = error ("Unexpected outcome " ++ (TL.unpack x))
+    toOutcome x
+      | x == "X" = Lose
+      | x == "Y" = Draw
+      | x == "Z" = Win
+      | otherwise = error ("Unexpected outcome " ++ (TL.unpack x))
 
 convertToRounds :: [(Move, Outcome)] -> [(Move, Move)]
 convertToRounds = map convertToRound
-    where
-        convertToRound (move, Draw) = (move, move)
+  where
+    convertToRound (move, Draw) = (move, move)
 
-        convertToRound (Rock, Win) = (Rock, Paper)
-        convertToRound (Rock, Lose) = (Rock, Scissors)
+    convertToRound (Rock, Win) = (Rock, Paper)
+    convertToRound (Rock, Lose) = (Rock, Scissors)
 
-        convertToRound (Paper, Win) = (Paper, Scissors)
-        convertToRound (Paper, Lose) = (Paper, Rock)
+    convertToRound (Paper, Win) = (Paper, Scissors)
+    convertToRound (Paper, Lose) = (Paper, Rock)
 
-        convertToRound (Scissors, Win) = (Scissors, Rock)
-        convertToRound (Scissors, Lose) = (Scissors, Paper)
+    convertToRound (Scissors, Win) = (Scissors, Rock)
+    convertToRound (Scissors, Lose) = (Scissors, Paper)
 
 scoreRounds :: [(Move, Move)] -> [Int]
 scoreRounds = map scoreRound
-    where
-        scoreRound (Rock, Rock) = rockPoints + tiePoints
-        scoreRound (Paper, Rock) = rockPoints + losePoints
-        scoreRound (Scissors, Rock) = rockPoints + winPoints
+  where
+    scoreRound (Rock, Rock) = rockPoints + tiePoints
+    scoreRound (Paper, Rock) = rockPoints + losePoints
+    scoreRound (Scissors, Rock) = rockPoints + winPoints
 
-        scoreRound (Rock, Paper) = paperPoints + winPoints
-        scoreRound (Paper, Paper) = paperPoints + tiePoints
-        scoreRound (Scissors, Paper) = paperPoints + losePoints
+    scoreRound (Rock, Paper) = paperPoints + winPoints
+    scoreRound (Paper, Paper) = paperPoints + tiePoints
+    scoreRound (Scissors, Paper) = paperPoints + losePoints
 
-        scoreRound (Rock, Scissors) = scissorsPoints + losePoints
-        scoreRound (Paper, Scissors) = scissorsPoints + winPoints
-        scoreRound (Scissors, Scissors) = scissorsPoints + tiePoints
+    scoreRound (Rock, Scissors) = scissorsPoints + losePoints
+    scoreRound (Paper, Scissors) = scissorsPoints + winPoints
+    scoreRound (Scissors, Scissors) = scissorsPoints + tiePoints
 
-        rockPoints = 1
-        paperPoints = 2
-        scissorsPoints = 3
-        losePoints = 0
-        tiePoints = 3
-        winPoints = 6
+    rockPoints = 1
+    paperPoints = 2
+    scissorsPoints = 3
+    losePoints = 0
+    tiePoints = 3
+    winPoints = 6
 
 puzzleInput :: PuzzleInput -> String
 puzzleInput Example = "./data/day02/example.txt"
